7548fd85284ab9fd4b9de1900e3f8a72dcd4f1b1
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-07-24 17:21:07 +0200

    fix: some stuff working

---
+++ packages/core/src/index.ts
@@ -0,0 +1,146 @@
+#!/usr/bin/env node
+import { constants } from "node:fs";
+import { readFile, access, readdir } from "node:fs/promises";
+import path from "node:path";
+import logSymbols from "log-symbols";
+import chalk from "chalk";
+
+import metascraper from "metascraper";
+import metascraperTitle from "metascraper-title";
+import metascraperImage from "metascraper-image";
+import metascraperDate from "metascraper-date";
+import metascraperDescription from "metascraper-description";
+import metascraperPublisher from "metascraper-publisher";
+import puppeteer, { Browser } from "puppeteer";
+
+interface Config {
+  buildDir: string;
+}
+
+const log = (() => {
+  const base = (level: string, ...message) =>
+    console.log(chalk.grey("ogimage"), level, ...message);
+
+  return {
+    info: (...message) => base(logSymbols.info, ...message),
+    success: (...message) => base(logSymbols.success, ...message),
+    warning: (...message) => base(logSymbols.warning, ...message),
+    error: (...message) => base(logSymbols.error, ...message),
+  };
+})();
+
+(async function run() {
+  if (await checkFileExists("ogimage.json")) {
+    const file = await readFile("ogimage.json", "utf8");
+    const config = JSON.parse(file) as Config;
+
+    log.info("Starting browser");
+    const browser = await startBrowser();
+
+    log.info("Looking for html files in", config.buildDir);
+    await walkPath(config.buildDir);
+
+    log.info("Stopping browser");
+    browser.close();
+    log.success("Browser stopped");
+  } else {
+    log.error("Did not find config file");
+  }
+})();
+
+function shouldExclude(dirOrFile: string): boolean {
+  const exclude = ["404", "500"].some((item) => dirOrFile.includes(item));
+  if (exclude) log.warning("Excluded", dirOrFile);
+  return exclude;
+}
+
+async function walkPath(basePath: string) {
+  if (shouldExclude(basePath)) return;
+
+  const pathContent = await readdir(basePath, { withFileTypes: true });
+
+  const files = pathContent
+    .filter((item) => item.isFile())
+    .filter((file) => path.extname(file.name).toLowerCase() == ".html")
+    .filter((file) => !shouldExclude(file.name))
+    .map((file) => file.name);
+
+  await Promise.allSettled(
+    files.map((file) => {
+      new Promise<void>(async (resolve, reject) => {
+        try {
+          const pathString = path.resolve(basePath, file);
+
+          const meta = await extractMeta(pathString);
+          await addOgImageTag(pathString);
+          await screenshot(meta);
+
+          log.success(
+            "Done",
+            pathString.replace(process.cwd(), "").substring(1)
+          );
+          resolve();
+        } catch (e) {
+          log.error("Failed to handle file", file, e.message);
+          reject(e);
+        }
+      });
+    })
+  );
+  const directories = pathContent
+    .filter((item) => !item.isFile())
+    .map((directory) => directory.name);
+
+  return await Promise.allSettled(
+    directories.map((directory) => {
+      new Promise<void>(async (resolve, reject) => {
+        try {
+          await walkPath(`${basePath}/${directory}`);
+          resolve();
+        } catch {
+          reject();
+        }
+      });
+    })
+  );
+}
+
+async function startBrowser() {
+  try {
+    const browser = await puppeteer.launch({
+      executablePath: process.env.PUPPETEER_EXEC_PATH,
+      headless: true,
+    });
+
+    log.success("Started browser");
+    return browser;
+  } catch (e) {
+    log.error("Failed to start browser", e.message);
+  }
+}
+
+async function extractMeta(pathString: string) {
+  const content = (await readFile(pathString)).toString();
+
+  const scraper = await metascraper([
+    metascraperTitle(),
+    metascraperImage(),
+    metascraperDate(),
+    metascraperDescription(),
+    metascraperPublisher(),
+  ]);
+
+  return await scraper({ url: null, html: content, validateUrl: false });
+}
+
+async function addOgImageTag(pathString: string) {
+  // TODO
+}
+
+async function screenshot(meta: any) {}
+
+function checkFileExists(file) {
+  return access(file, constants.F_OK)
+    .then(() => true)
+    .catch(() => false);
+}
