35d642035f6342f478b39036eb726c3cf0852118
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-08-13 16:08:56 +0200

    Browser tests

--- packages/core/__tests__/browser.spec.ts
+++ packages/core/__tests__/browser.spec.ts
@@ -2,7 +2,29 @@ import startBrowser from "../src/browser.js";
 
 test("it starts", async () => {
   const browser = await startBrowser();
-  await expect(browser).toBeDefined();
+  expect(browser).toBeDefined();
+
+  await browser.close();
+});
+
+test("it can open a page", async () => {
+  const browser = await startBrowser();
+  await browser.newPage();
+  await browser.close();
+});
+
+test("it can open load an url", async () => {
+  const browser = await startBrowser();
+  const page = await browser.newPage();
+
+  const loadUrl = "https://example.com";
+  await page.goto(loadUrl, { waitUntil: "domcontentloaded" });
+
+  const returnedUrl = await page.evaluate(() => {
+    return window.location.origin;
+  });
+
+  expect(returnedUrl).toBe(loadUrl);
 
   await browser.close();
 });
